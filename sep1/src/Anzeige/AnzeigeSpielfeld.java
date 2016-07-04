f05f1f2c597ad1f833f05ec3551c45d75d144865
leifii <mark.leifeld@stud.uni-duisburg-essen.de>
2016-07-04 10:52:20 +0200

    Update AnzeigeSpielfeld.java

--- sep1/src/Anzeige/AnzeigeSpielfeld.java
+++ sep1/src/Anzeige/AnzeigeSpielfeld.java
@@ -20,6 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.lang.*;
 import java.math.*;
+import Positionen.Position;
 
 public class AnzeigeSpielfeld extends JFrame {
 
@@ -27,6 +28,15 @@ public class AnzeigeSpielfeld extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	ImageIcon iiSepman=new ImageIcon("Sepman.png");                     // Hinzugefügt von Mark
+	JLabel jSepman = new JLabel(iiSepman);                              // Hinzugefügt von Mark
+	ImageIcon iiRandom=new ImageIcon("zufall.png");                      // Hinzugefügt von Mark
+	JLabel jRandom = new JLabel(iiRandom);                              // Hinzugefügt von Mark
+	ImageIcon iiGuard=new ImageIcon("Verteidiger.png");                  // Hinzugefügt von Mark 
+	JLabel jGuard = new JLabel(iiGuard);                                // Hinzugefügt von Mark
+	ImageIcon iiTracker=new ImageIcon("Verfolger.png");                  // Hinzugefügt von Mark
+	JLabel jTracker = new JLabel(iiTracker);                            // Hinzugefügt von Mark
+//	JLabel[] ele= new JLabel[40];
 	public JPanel contentPane;
 	 Plane[] Spielbrett;
 	 int[] pos=new int[4];
@@ -228,7 +238,6 @@ public class AnzeigeSpielfeld extends JFrame {
 					i++;
 				}
 					j=new JLabel(iiKnoten); // Hinzugefügt von Mark 
-					j.setBackground(Color.BLACK);
 					j.setBounds(100+k*150,100+i*150,55,55);
 					contentPane.add(j);
 				zahl++;
@@ -238,18 +247,10 @@ public class AnzeigeSpielfeld extends JFrame {
 		return ele;
 	}
 	
-	
 	public AnzeigeSpielfeld(String a,JPanel panel) throws IOException {
-		
-		
 		create(a);
 		
-		
-		
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
 		
 		setBounds(100, 100, (Länge+1)*150, (Breite+1)*150);
 		contentPane = panel;
@@ -258,39 +259,20 @@ public class AnzeigeSpielfeld extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel[] ele=erzeugeSpielfeld();
-	/*			EventQueue.invokeLater(new Runnable() {
-		public void run() {
-			try {
-				AnzeigeSpielfeld frame = new AnzeigeSpielfeld();
-				frame.setVisible(true);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	});*/
-		//haha.setVisible(true);
+		
 		/*
 		 * Hinzufügen des Sepmanns und der Geister
 		 */
 
-		ImageIcon iiSepman=new ImageIcon("Sepman.png");                     // Hinzugefügt von Mark
-		JLabel jSepman = new JLabel(iiSepman);                              // Hinzugefügt von Mark
 		jSepman.setBounds(ele[Sepman].getX() , ele[Sepman].getY(), 65, 65);
 		contentPane.add(jSepman);
 
-		
-		ImageIcon iiRandom=new ImageIcon("zufall.png");                      // Hinzugefügt von Mark
-		JLabel jRandom = new JLabel(iiRandom);                              // Hinzugefügt von Mark
 		jRandom.setBounds(ele[Random].getX() , ele[Random].getY(), 60, 55);
 		contentPane.add(jRandom);
 		
-		ImageIcon iiGuard=new ImageIcon("Verteidiger.png");                  // Hinzugefügt von Mark 
-		JLabel jGuard = new JLabel(iiGuard);                                // Hinzugefügt von Mark
 		jGuard.setBounds(ele[Defender].getX() , ele[Defender].getY(), 60, 55);
 		contentPane.add(jGuard);
 		
-		ImageIcon iiTracker=new ImageIcon("Verfolger.png");                  // Hinzugefügt von Mark
-		JLabel jTracker = new JLabel(iiTracker);                            // Hinzugefügt von Mark
 		jTracker.setBounds(ele[Tracer].getX() , ele[Tracer].getY(), 60, 55);
 		contentPane.add(jTracker); 
 		
@@ -360,9 +342,84 @@ public class AnzeigeSpielfeld extends JFrame {
 			
 				
 		}
+	}
+	
+	public void bewegeRoboter(){	
+		int[][] pos = new int[Größe][2];
+		int k = 0 ; 
+		int j = 0 ; 
+        for ( int i = 0 ; i<40 ; i++){
+        	if(k>=Breite)
+			{
+				k=0;
+				j++;
+			}
+        	pos[i][0]=100+k*150;
+        	pos[i][1]= 100+j*150;
+        	k++;
+        	
+        }
+		int g1 = Position.getPosSepman();
+		g1 = convertKnoten(g1);
+		int g2 = Position.getPosTracer();
+		g2 = convertKnoten(g2);
+		int g3 = Position.getPosDefender();
+		g3 = convertKnoten(g3);
+		int g4 = Position.getPosRandom();
+		g4 = convertKnoten(g4);
 		
+		this.jGuard.setLocation(pos[1][0], pos[1][1]);
+		
+		this.jRandom.setLocation(pos[2][0], pos[2][1]);
+		
+		this.jSepman.setLocation(pos[3][0], pos[3][1]);
+		
+		this.jTracker.setLocation(pos[4][0], pos[4][1]); 
 		
 		
 		
 	}
+	
+	
+	public int convertKnoten(int i){
+		switch(i){
+		case 01 : return 1;
+		case 02 : return 2;
+		case 03 : return 3;
+		case 04 : return 4;
+		case 05 : return 5 ;
+		case 06 : return 6;
+		case 11 : return 7;
+		case 12 : return 8;	
+		case 13 : return 9;	
+		case 14 : return 10;	
+		case 15 : return 11;
+		case 16 : return 12;
+		case 21 : return 13 ;
+		case 22 : return 14 ;
+		case 23 : return 15;
+		case 24 : return 16 ;
+		case 25 : return 17;
+		case 26 : return 18 ;
+		case 31 : return 19;
+		case 32 : return 20 ;
+		case 33 : return 21 ;
+		case 34 : return 22 ;
+		case 35 : return 23 ;
+		case 36 : return 24 ;
+		case 41 : return 25;
+		case 42 : return 26 ;
+		case 43 : return 27;
+		case 44 : return 28;		
+		case 45 : return 29;		
+		case 46 : return 30 ;		
+		case 51 : return 31 ;	
+		case 52 : return 32 ;		
+		case 53 : return 33;	
+		case 54 : return 24;
+		case 55 : return 35 ;
+		case 56 : return 36 ;
+		}
+		return 0;
+	}	
 }
